3cc78eabad1b8e79556c33b7dc86868a0f0e5219
noppi401 <noppi623@gmail.com>
2026-05-14 10:15:38 +0900

    feat: add execution manager lifecycle state machine

---
+++ src/aivenv/execution/manager.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+import asyncio
+import contextlib
+import inspect
+import logging
+import shYѥ�ɕ��ٔ(����ɽ�йɵ��Ƞ�(�����݅�Ё��幍���ѽ}ѡɕ���ɕ��ٔ�(()��幌�����}��剕}�݅�Сم�Ք��䤀������(�������������й�݅ͅ�х����م�Ք��(��������ɕ��ɸ��݅�Ёم�Ք(����ɕ��ɸ�م�Ք(
